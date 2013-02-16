3508ea2815999ab704b4cd79448933589579e77e
Jacob Norlin Andersson <jacob.norlinandersson@gmail.com>
2013-02-16 15:48:27 +0100

    inget viktigt

--- src/com/NewApp/ZyphyrListener.java
+++ src/com/NewApp/ZyphyrListener.java
@@ -1,8 +1,14 @@
 package com.NewApp;
 
+import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import zephyr.android.HxMBT.ConnectListenerImpl;
 import zephyr.android.HxMBT.ConnectedEvent;
+import zephyr.android.HxMBT.ZephyrPacketArgs;
+import zephyr.android.HxMBT.ZephyrPacketEvent;
+import zephyr.android.HxMBT.ZephyrPacketListener;
+import zephyr.android.HxMBT.ZephyrProtocol;
 
 public class ZyphyrListener extends ConnectListenerImpl {
 	
@@ -16,13 +22,61 @@ public class ZyphyrListener extends ConnectListenerImpl {
 	//==
 	
 	private Handler zephyrHandler;
+	private HRSpeedDistPacketInfo HRSpeedDistPacket = new HRSpeedDistPacketInfo();
 
 	public ZyphyrListener(Handler handler, Handler zephyrHandler) {
 		super(handler, null);
 		this.zephyrHandler = zephyrHandler;
 	}
-	
 	public void Connected(ConnectedEvent<BTClient> eventArgs){
+		
+		System.out.println(String.format("Connected to BioHarness %s.", eventArgs.getSource().getDevice().getName()));
+
+
+
+
+
+
+		//Creates a new ZephyrProtocol object and passes it the BTComms object
+		ZephyrProtocol protocol = new ZephyrProtocol(eventArgs.getSource().getComms());
+		//ZephyrProtocol _protocol = new ZephyrProtocol(eventArgs.getSource().getComms(), );
+		protocol.addZephyrPacketEventListener(new ZephyrPacketListener() {
+			public void ReceivedPacket(ZephyrPacketEvent eventArgs) {
+				ZephyrPacketArgs msg = eventArgs.getPacket();
+				byte CRCFailStatus;
+				byte RcvdBytes;
+
+
+
+				CRCFailStatus = msg.getCRCStatus();
+				RcvdBytes = msg.getNumRvcdBytes() ;
+				if (HR_SPD_DIST_PACKET==msg.getMsgID())
+				{
+
+
+					byte [] DataArray = msg.getBytes();
+
+					//***************Displaying the Heart Rate********************************
+					int HRate =  HRSpeedDistPacket.GetHeartRate(DataArray);
+					Message text1 = zephyrHandler.obtainMessage(HEART_RATE);
+					Bundle b1 = new Bundle();
+					b1.putString("HeartRate", String.valueOf(HRate));
+					text1.setData(b1);
+					zephyrHandler.sendMessage(text1);
+					System.out.println("Heart Rate is "+ HRate);
+
+					//***************Displaying the Instant Speed********************************
+					double InstantSpeed = HRSpeedDistPacket.GetInstantSpeed(DataArray);
+
+					text1 = zephyrHandler.obtainMessage(INSTANT_SPEED);
+					b1.putString("InstantSpeed", String.valueOf(InstantSpeed));
+					text1.setData(b1);
+					zephyrHandler.sendMessage(text1);
+					System.out.println("Instant Speed is "+ InstantSpeed);
+
+				}
+			}
+		});
 		
 	}
 
